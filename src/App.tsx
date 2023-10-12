880824401c81b3674c06f2fd5893ed476171cb1d
deyamarco <marek.deja@yahoo.com>
2023-10-12 22:57:46 +0200

    First part of creating app finished. Problem with alias resolve

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Button from '@components/Button/Button'
+
+function App(): JSX.Element {
+   return <Button />
+}
+
+export default App
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import buttonStyles from 'Button.module.scss'
+
+const Button = () => {
+   return (
+      <button className={buttonStyles.button}>
+         <span className={buttonStyles.label}>Button</span>
+      </button>
+   )
+}
+
+export default Button
